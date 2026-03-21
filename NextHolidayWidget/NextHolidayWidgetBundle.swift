fc119a0097a0ff4f68dd8b335b7d48108ad706bd
LackFos <lackfosisonline@gmail.com>
2026-03-21 14:39:38 +0700

    feat(KP-2): add static widget layout

---
+++ NextHolidayWidget/NextHolidayWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NextHolidayWidgetBundle.swift
+//  NextHolidayWidget
+//
+//  Created by Elvis on 20/03/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NextHolidayWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NextHolidayWidget()
+    }
+}
